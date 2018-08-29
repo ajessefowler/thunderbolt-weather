d29a0b23963719141a7ad4e6309d4dd39c832a85
ajessefowler <ajessefowler@gmail.com>
2018-08-29 18:21:27 -0400

    Prevent body scroll when scrolling through menu

--- js/animations.js
+++ js/animations.js
@@ -53,6 +53,11 @@ document.addEventListener('DOMContentLoaded', function(event) {
 		document.getElementById('locationname').addEventListener('click', function() {
 			zenscroll.to(document.getElementById('locationname'));
 		});
+
+		// Prevent body when scrolling through menu
+		document.getElementById('historycard').addEventListener('touchmove', function(e) {
+			e.preventDefault();
+		}, false);
 	}
 });
 
